edea09a7429737e3cff08da7e4e25edaac2cd376
Jason Heredia <heredijr@gmatc.matc.edu>
2021-11-29 22:20:32 -0600

    Completed Activity 9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,23 @@
-import axios from 'axios';
 import React from 'react';
+import './App.css';
+import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PostList from './components/PostList/PostList';
 import Post from './components/Post/Post';
-import './App.css';
 
 class App extends React.Component {
   state = {
-    posts: [], 
+    posts: [],
     post: null
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000/api/posts')
-    .then((response) => {
-      this.setState({
-        posts: response.data
+      .then((response) => {
+        this.setState({
+          posts: response.data
+        })
       })
-    })
       .catch((error) => {
         console.error(`Error fetching data: ${error}`);
       })
@@ -30,6 +30,20 @@ class App extends React.Component {
     });
   }
 
+  deletePost = post => {
+    axios
+      .delete(`http://localhost:5000/api/posts/${post.id}`)
+      .then(response => {
+        const newPosts = this.state.posts.filter(p => p.id !== post.id);
+        this.setState({
+          posts: [...newPosts]
+        });
+      })
+      .catch(error => {
+        console.error(`Error deleting post: ${error}`);
+      });
+  };
+
   render() {
     const { posts, post } = this.state;
 
@@ -38,11 +52,15 @@ class App extends React.Component {
         <div className="App">
           <header className="App-header">
             BlogBox
-          </header>
+        </header>
           <main className="App-content">
             <Switch>
-              <Route path="/">
-                <PostList posts={posts} clickPost={this.viewPost} />
+              <Route exact path="/">
+                <PostList 
+                  posts={posts} 
+                  clickPost={this.viewPost}
+                  deletePost={this.deletePost}
+                />
               </Route>
               <Route path="/posts/:postId">
                 <Post post={post} />
